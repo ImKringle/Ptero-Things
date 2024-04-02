444b0c840aa908a5181702c52c4e27f8bd98ce85
Dylan <135805091+ImKringle@users.noreply.github.com>
2024-04-01 23:47:38 -0400

    Add Tux Entrypoint

---
+++ images/tuxlauncher/entrypoint.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+cd /home/container
+
+# Color Codes
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+# Information output
+echo "Running on Debian $(cat /etc/debian_version)"
+echo "Current timezone: $(cat /etc/timezone)"
+wine --version
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Check if auto_update is not set or set to 1 for update
+if [ -z "${AUTO_UPDATE}" ] || [ "${AUTO_UPDATE}" == "1" ]; then
+    # Update the Server
+    git config pull.rebase false
+    git pull
+
+    # Check if requirements.txt exists
+    if [[ -f "/home/container/requirements.txt" ]]; then
+        # Install/upate requirements
+        pip install -U --prefix .local -r /home/container/requirements.txt
+    else
+        # Display error if requirements.txt does not exist
+        echo -e "\t${RED}No requirements.txt found. Installation failed.${NC}"
+    fi
+else
+    echo -e "\t${GREEN}Not updating game server as auto update was set to 0. Starting Server${NC}"
+fi
+
+if [[ $XVFB == 1 ]]; then
+    Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
+fi
+
+# Install necessary to run packages
+echo "\t${YELLOW}First launch will throw some errors. Ignore them${NC}"
+
+mkdir -p $WINEPREFIX
+
+# Check if wine-gecko required and install it if so
+if [[ $WINETRICKS_RUN =~ gecko ]]; then
+        echo "Installing Gecko"
+        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
+
+        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
+                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
+        fi
+
+        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
+                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
+        fi
+
+        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
+        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
+fi
+
+# Check if wine-mono required and install it if so
+if [[ $WINETRICKS_RUN =~ mono ]]; then
+        echo "Installing mono"
+        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
+
+        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
+                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
+        fi
+
+        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+fi
+
+# List and install other packages
+for trick in $WINETRICKS_RUN; do
+        echo "Installing $trick"
+        winetricks -q $trick
+done
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+echo -e "\n${GREEN}[STARTUP]:${NC} Starting server with the following startup command:"
+eval ${MODIFIED_STARTUP}
+
+if [ $? -ne 0 ]; then
+    echo -e "\n${RED}PTDL_CONTAINER_ERR: There was an error while attempting to run the start command.${NC}\n"
+    exit 1
+fi
